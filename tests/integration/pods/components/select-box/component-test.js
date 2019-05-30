021be423ac2b499d599b42b13fd84abdefe34996
Matt Horn <mathorn@gmx.de>
2019-05-30 20:34:19 +0200

    skip tests :)

--- tests/integration/pods/components/select-box/component-test.js
+++ tests/integration/pods/components/select-box/component-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, skip } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('question-item', 'Integration | Component | question item', {
+moduleForComponent('select-box', 'Integration | Component | select box', {
   integration: true
 });
 
@@ -9,15 +9,15 @@ skip('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{question-item}}`);
+  this.render(hbs`{{select-box}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#question-item}}
+    {{#select-box}}
       template block text
-    {{/question-item}}
+    {{/select-box}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
