96c9f144d3059a77d495b879ffd0a75aff7e32da
Matt Horn <mathorn@gmx.de>
2019-05-30 18:20:48 +0200

    load questions

--- app/routes/questions.js
+++ app/routes/questions.js
@@ -1,4 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  // don't use model hook to load model, as it is blocking page rendering
+  setupController(controller) {
+    this.store.findAll('question').then( response => {
+      controller.set('model', response);
+    }, error => {
+      controller.set('error', true);
+    });
+  }
 });
